876de65cf2109f1ed8db5c2f898995db63e4fd4f
nazar <nazikkln@gmail.com>
2020-12-09 20:06:21 +0100

    Primo Push

---
+++ Esercitazione02/src/main/java/it/unical/mat/webcomp21/secreteria/Esercitazione02Application.java
@@ -0,0 +1,13 @@
+package it.unical.mat.webcomp21.secreteria;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Esercitazione02Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(Esercitazione02Application.class, args);
+	}
+
+}
---
+++ Esercitazione02/src/test/java/it/unical/mat/webcomp21/secreteria/Esercitazione02ApplicationTests.java
@@ -0,0 +1,13 @@
+package it.unical.mat.webcomp21.secreteria;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Esercitazione02ApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
